b9547c19c970fa5be1592f4f658264cc8679c102
Ashwin Balaji <s.ashwinbalaji.de@gmail.com>
2023-09-16 10:16:01 +0200

    recursive functions

--- test.hs
+++ test.hs
@@ -30,3 +30,18 @@ bmiTell2 weight height
           skinny = 18.5  
           normal = 25.0  
           fat = 30.0
+
+maximum' :: (Ord a) => [a] -> a  
+maximum' [] = error "maximum of empty list"  
+maximum' [x] = x  
+maximum' (x:xs)   
+    | x > maxTail = x  
+    | otherwise = maxTail  
+    where maxTail = maximum' xs  
+
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
