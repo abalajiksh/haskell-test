4a9a689db6ce8e0232aad34e8ddb24152970d60f
Ashwin Balaji <s.ashwinbalaji.de@gmail.com>
2023-09-16 10:50:42 +0200

    Update test.hs

--- test.hs
+++ test.hs
@@ -45,3 +45,4 @@ quicksort (x:xs) =
     let smallerSorted = quicksort [a | a <- xs, a <= x]  
         biggerSorted = quicksort [a | a <- xs, a > x]  
     in  smallerSorted ++ [x] ++ biggerSorted  
+
